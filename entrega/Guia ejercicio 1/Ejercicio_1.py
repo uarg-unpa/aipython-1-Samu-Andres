90cc66b148d169e56c832b53e45c549f8a107f65
samuel <111392600+Samu-Andres@users.noreply.github.com>
2023-10-09 22:51:06 +0000

    hacer 2

---
+++ entrega/Guia ejercicio 1/Ejercicio_1.py
@@ -0,0 +1,65 @@
+#1.	Mostrar un mensaje y una línea vacía:
+
+print("Las máquinas me sorprenden con mucha frecuencia.")
+print()  # Línea vacía
+
+#2.	Mostrar tus propios mensajes:
+
+print("Hola, soy un programa en Python.")
+print("Estoy aquí para ayudarte.")
+
+#3.	Mostrar el número “23” y 23:
+
+print("23")  # Esto imprimirá el texto "23"
+print(23)    # Esto imprimirá el número 23
+
+#4.	Mostrar un mensaje con comillas incluidas:
+
+print('Una computadora puede ser llamada "inteligente" si logra engañar a una persona haciéndole creer que es un humano.')
+
+#5.	Mostrar nombre, apellido y edad separados por espacios:
+
+nombre1 = "Tu"
+apellido1 = "Apellido"
+edad1 = 30
+print(nombre1, apellido1, edad1)
+
+#6.	Mostrar nombre, apellido y edad separados por guión bajo utilizando el argumento sep:
+
+nombre2 = "Tu"
+apellido2 = "Apellido"
+edad2 = 30
+print(nombre2, apellido2, edad2, sep="_")
+
+#7.	Mostrar calle, número y código postal separados por tabulación:
+
+calle1 = "Calle Principal"
+numero1 = "123"
+codigo_postal1 = "12345"
+print(calle1, numero1, codigo_postal1, sep="\t")
+
+#8.	Mostrar cada argumento en una nueva línea:
+
+print(calle1)
+print(numero1)
+print(codigo_postal1)
+
+#9.	Imprimir un texto con caracteres especiales para formar una escalera:
+
+texto = "Feliz\n\t\tPrimavera\n\t\t\t2023"
+print(texto)
+
+#10.	Imprimir dos frases en una sola línea:
+
+print("Solo podemos ver poco del futuro,", end=" ")
+print("pero lo suficiente para darnos cuenta de que hay mucho que hacer.")
+
+#11. Dibujar el siguiente arbol:
+
+print("   *")
+print("  * *")
+print(" *   *")
+print("*     *")
+print(" *   *")
+print(" *   *")
+print(" *****")
