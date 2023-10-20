d62ee9d88878dadcb14956f64eff89a4b5cbaa16
samuel <111392600+Samu-Andres@users.noreply.github.com>
2023-10-20 21:48:01 +0000

    actualizo3

---
+++ entrega/Guia ejercicio 1/Ejercicio_3.py
@@ -0,0 +1,38 @@
+# Tu nombre
+nombre = "Samuel"
+
+# Tu apellido
+apellido = "Herrera"
+
+# Tu edad
+edad = 24  
+
+# Tu altura (en metros, por ejemplo)
+altura = 1.75  
+
+# Un número de vuelo
+numero_vuelo = "XYHBU"
+
+# La temperatura del ambiente (en grados Celsius, por ejemplo)
+temperatura_ambiente = 25.5
+
+# El salario mensual (en dólares, por ejemplo)
+salario_mensual = 50000  
+
+# Para representar que el juego terminó (usando un booleano)
+juego_terminado = True  
+
+# Para determinar si un número es par (usando un número entero)
+numero = 25  
+es_par = numero % 2 == 0  
+
+# Puedes imprimir las variables para verificar su contenido
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("Edad:", edad)
+print("Altura:", altura)
+print("Número de vuelo:", numero_vuelo)
+print("Temperatura del ambiente:", temperatura_ambiente)
+print("Salario mensual:", salario_mensual)
+print("Juego terminado:", juego_terminado)
+print("Es par:", es_par)
