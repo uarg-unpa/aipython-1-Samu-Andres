8bb5109f52eb4b1a512018f1a96298a211f37dd7
samuel <111392600+Samu-Andres@users.noreply.github.com>
2023-10-20 21:22:20 +0000

    actualizado

---
+++ entrega/test.py
@@ -0,0 +1,14 @@
+print("Viernes en AIPYTHON")
+print()
+print("taller", "AIPYTHON", "viernes")
+print()
+print("taller", "AIPYTHON", "viernes", sep="", end="\n")
+# \n \t no los toma como caracter
+# end"_"
+print("dias\t topico \t ejercicios")
+print()
+print("La funcion \"print\" ...")
+# "\\"
+print('La funcion "print" ...')
+# '' 
+#""
