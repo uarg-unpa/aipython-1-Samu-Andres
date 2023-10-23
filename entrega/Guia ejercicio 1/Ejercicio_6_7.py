559c5ad24679a6ff01cb272dd41ea110b8b0d063
samuel <111392600+Samu-Andres@users.noreply.github.com>
2023-10-23 23:13:08 +0000

    Hecho hasta el 7

---
+++ entrega/Guia ejercicio 1/Ejercicio_6_7.py
@@ -0,0 +1,34 @@
+#EJERCICIO 6
+# Solicito dos números enteros al usuario
+num1 = int(input("Introduce el primer número entero: "))
+num2 = int(input("Introduce el segundo número entero: "))
+
+# Se realiza las operaciones
+suma = num1 + num2
+resta = num1 - num2
+producto = num1 * num2
+potencia = num1 ** num2
+resto = num1 % num2
+
+# Muestro los resultados
+print(f"La suma de {num1} y {num2} es: {suma}")
+print(f"La resta de {num1} y {num2} es: {resta}")
+print(f"El producto de {num1} y {num2} es: {producto}")
+print(f"{num1} elevado a la {num2} es: {potencia}")
+print(f"El resto de {num1} dividido por {num2} es: {resto}")
+
+#EJERCICIO 7
+num1 = int(input("Introduce un número entero: "))
+num2 = float(input("Introduce un número decimal: "))
+
+suma = num1 + num2
+resta = num1 - num2
+producto = num1 * num2
+potencia = num1 ** num2
+resto = num1 % num2
+
+print(f"La suma de {num1} y {num2} es: {suma}")
+print(f"La resta de {num1} y {num2} es: {resta}")
+print(f"El producto de {num1} y {num2} es: {producto}")
+print(f"{num1} elevado a la {num2} es: {potencia}")
+print(f"El resto de {num1} dividido por {num2} es: {resto}")
