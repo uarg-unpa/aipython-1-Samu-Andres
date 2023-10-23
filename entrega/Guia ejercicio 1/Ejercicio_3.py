e0580a0d021cdbe2c7bca3b94dc35deb1472667e
samuel <111392600+Samu-Andres@users.noreply.github.com>
2023-10-23 23:02:59 +0000

    Hecho ejercicio 3

---
+++ entrega/Guia ejercicio 1/Ejercicio_3.py
@@ -0,0 +1,20 @@
+# EJERCICIO 3
+mi_nombre = "Samuel"
+mi_apellido = "Herrera"
+mi_edad = 24
+mi_altura = 1.73
+numero_vuelo = "ABC123"
+temperatura_ambiente = 25.5
+salario_mensual = 5000.00
+juego_terminado = True
+numero = 3
+es_numero_par = numero % 2 == 0
+
+print(mi_nombre, mi_apellido)
+print(mi_edad)
+print(mi_altura)
+print(numero_vuelo)
+print(temperatura_ambiente)
+print(salario_mensual)
+print(juego_terminado)
+print(es_numero_par)
